8d8a142c39bb85c325c32ccf0cf2f7f5b9e0f5e4
gedas-k <gediminas.kaknevicius@gmail.com>
2020-07-29 14:41:46 +0300

    Added Images

--- index.php
+++ index.php
@@ -1,6 +1,57 @@
 <?php
 $title = "Home";
-$content = "Hello World";
+$content = '
+<h3>…or create a new repository on the command line</h3>
+<p>
+…or create a new repository on the command line
+echo "# front-page" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+                
+…or push an existing repository from the command line
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
+</p>
+
+<h3>…or push an existing repository from the command line</h3>
+<p>
+…or create a new repository on the command line
+echo "# front-page" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+                
+…or push an existing repository from the command line
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
+</p>
+
+<h3>…or import code from another repository</h3>
+<p>
+…or create a new repository on the command line
+echo "# front-page" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+                
+…or push an existing repository from the command line
+git remote add origin https://github.com/gedas-k/front-page.git
+git push -u origin master
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
+</p>
+';
 
 include 'template.php';
 
